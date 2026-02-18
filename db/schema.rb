2a676d5bd3380aa0289c63e559bfa89553d228e8
kanazawa <kanazawa@knzw-mbp.local>
2026-02-18 11:23:13 +0900

    add normal migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_18_020917) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_18_022203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,6 +58,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_18_020917) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.integer "age"
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
